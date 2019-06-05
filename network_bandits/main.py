2b79f2bc7d89c9938d5a59e8fe940cbed908548c
Abhimanyu Dubey <abhimanyu1401@gmail.com>
2019-06-05 15:36:03 -0400

    completed networkbandit class

--- network_bandits/main.py
+++ network_bandits/main.py
@@ -2,6 +2,27 @@ import numpy as np
 import networkx as nx
 
 
+def cms_alpha(_alpha, _beta, mu, sigma):
+    ''' Generate a random sample from the alpha-stable distribution f
+        using the CMS Algorithm, where f : S_alpha(beta, mu, sigma) '''
+
+    b = np.arctan(_beta * np.tan(0.5 * np.pi * _alpha))*(1/_alpha)
+    s = (1 + (_beta * np.tan(0.5 * np.pi * _alpha))**2)**(0.5/_alpha)
+
+    v = np.random.uniform(-0.5*np.pi, 0.5*np.pi)
+    w = np.random.exponential(1)
+
+    if _alpha == 1:
+        z = 2/np.pi * ((
+            0.5*np.pi + _beta*v)*np.tan(v) - _beta *
+            np.log((np.pi*0.5*w*np.cos(v))/(0.5*np.pi + _beta*v)))
+    else:
+        z = s * (np.sin(_alpha*(v + b))/(np.cos(v)**(1/_alpha))) *\
+            (np.cos(v - _alpha*(v + b))/w)**((1-_alpha)/_alpha)
+
+    return float(z*sigma + mu)
+
+
 class NetworkBandit:
 
     @staticmethod
@@ -137,6 +158,57 @@ class NetworkBandit:
                     mu_min, mu_max,
                     (self.num_agents, self.num_arms))
 
+        # setting variance parameters based on distribution
+        if self.dist == 'gaussian':
+            if 'sigma' in kwargs:
+                if type(kwargs['sigma']) in [int, float]:
+                    self.sigma = np.full_like(self.means, kwargs['sigma'])
+                elif type(kwargs['sigma']) is list:
+                    assert len(kwargs['sigma']) == self.num_agents
+                    assert len(kwargs['sigma'][0]) == self.num_arms
+                    self.sigma = np.array(kwargs['sigma'])
+                elif type(kwargs['sigma']) is np.ndarray:
+                    assert kwargs['sigma'].shape ==\
+                        [self.num_agents, self.num_arms]
+                    self.sigma = kwargs['sigma']
+            else:
+                self.sigma = np.full_like(self.means, 1)
+
+        elif self.dist == 'stable':
+            if 'alpha' in kwargs:
+                if type(kwargs['alpha']) in [int, float]:
+                    assert kwargs['alpha'] > 1
+                    self.alpha = np.full_like(self.means, kwargs['alpha'])
+                elif type(kwargs['alpha']) is list:
+                    assert len(kwargs['alpha']) == self.num_agents
+                    assert len(kwargs['alpha'][0]) == self.num_arms
+                    assert all(np.greater(np.array(kwargs['alpha']), 1))
+                    self.alpha = np.array(kwargs['alpha'])
+                elif type(kwargs['alpha']) is np.ndarray:
+                    assert kwargs['alpha'].shape ==\
+                        [self.num_agents, self.num_arms]
+                    assert all(np.greater(kwargs['alpha'], 1))
+                    self.alpha = kwargs['alpha']
+            else:
+                self.alpha = np.full_like(self.means, 2)
+            if 'sigma' in kwargs:
+                if type(kwargs['sigma']) in [int, float]:
+                    self.sigma = np.full_like(self.means, kwargs['sigma'])
+                elif type(kwargs['sigma']) is list:
+                    assert len(kwargs['sigma']) == self.num_agents
+                    assert len(kwargs['sigma'][0]) == self.num_arms
+                    self.sigma = np.array(kwargs['sigma'])
+                elif type(kwargs['sigma']) is np.ndarray:
+                    assert kwargs['sigma'].shape ==\
+                        [self.num_agents, self.num_arms]
+                    self.sigma = kwargs['sigma']
+            else:
+                self.sigma = np.full_like(self.means, 1)
+
+        self.average_rewards = np.zeros_like(self.means)
+        self.pulls = np.zeros_like(self.means)
+        self.iter = 0
+
     def verifyInit(self):
         # verify if the assignments are all correct
         if self.eps is None:
@@ -153,11 +225,66 @@ class NetworkBandit:
 
             return True
 
+    def getRewardSample(self, agent_id, arm):
+        ''' Generate rewards for agent (randomly sampled)'''
+
+        if self.dist is 'gaussian':
+            reward = np.random.normal(
+                self.means[agent_id][arm], self.sigma[agent_id][arm], 1)
+        elif self.dist is 'bernoulli':
+            reward = np.random.binomial(1, self.means[agent_id][arm])
+        elif self.dist is 'stable':
+            reward = cms_alpha(
+                self.alpha[agent_id][arm], 0,
+                self.means[agent_id][arm], self.sigma[agent_id][arm])
+
+        self.average_rewards[agent_id][arm] = (
+            self.average_rewards[agent_id][arm]*self.pulls[agent_id][arm] +
+            reward)/(self.pulls[agent_id][arm]+1.0)
+        self.pulls[agent_id][arm] += 1.0
+
+        return reward
+
+    def getRegret(self):
+        '''Calculate the overall regret at any iteration of the game.'''
+
+        obtained_rewards = np.sum(
+            np.multiply(self.pulls, self.average_rewards))
+        best_rewards = np.sum(np.max(self.means, axis=1)*self.iter)
+
+        return best_rewards - obtained_rewards
+
+    def getNeighborInformation(self, src):
+        '''Communicate information about neighbor rewards.'''
+
+        averages, counts = [], []
+        for arm in range(self.num_arms):
+
+            arm_averages, arm_counts = [], []
+            for dest in self.graphs[arm].neighbors(src):
+                arm_averages.append(self.average_rewards[dest][arm])
+                arm_counts.append(self.counts[dest][arm])
+
+            averages.append(arm_averages)
+            counts.append(arm_counts)
+
+        return averages, counts
+
 
 class NetworkAgent:
 
-    def __init__(self):
-        return None
+    def __init__(self, graphs, id):
+
+        self.trials = 0
+        self.num_arms = len(graphs)
+        self.id = id
+        self.neighbors = [list(g.neighbors(id) for g in graphs)]
+
+
+class BaseUCBAgent(NetworkAgent):
+
+    def play(self):
+        pass
 
 
 if __name__ == '__main__':
