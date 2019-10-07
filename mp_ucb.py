c94b218edd453bfc4bd04d38c8c8dbd315c35439
Abhimanyu Dubey <abhimanyu1401@gmail.com>
2019-10-07 14:46:08 -0400

    MP-UCB

--- mp_ucb.py
+++ mp_ucb.py
@@ -48,7 +48,7 @@ def generate_connected_graph(n, graph_type='er'):
     if graph_type == 'er':
         main_graph = nx.gnp_random_graph(n, 0.05)
 
-    for edge in base_path_graph:
+    for edge in base_path_graph.edges:
         if not main_graph.has_edge(*edge):
             main_graph.add_edge(*edge)
 
